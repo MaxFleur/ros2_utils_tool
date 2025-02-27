297ad2be22fe31c04ca60d4ed407a392968e34cf
Maxime Fleury <bakefish@web.de>
2025-02-27 10:07:27 +0100

    [version][ui] Adjust to v0.9.0

--- src/ui/StartWidget.cpp
+++ src/ui/StartWidget.cpp
@@ -136,8 +136,8 @@ StartWidget::StartWidget(Parameters::DialogParameters& dialogParameters, QWidget
     backButtonLayout->addWidget(m_backButton);
     backButtonLayout->addStretch();
 
-    m_versionLabel = new QLabel("v0.8.0");
-    m_versionLabel->setToolTip("Merge bags, scale published image topics\nand various UI and CLI improvements!");
+    m_versionLabel = new QLabel("v0.9.0");
+    m_versionLabel->setToolTip("Bag to pcds/pcds to bag tool and more error messages for CLI tools!");
 
     auto* const versionLayout = new QHBoxLayout;
     versionLayout->addStretch();
