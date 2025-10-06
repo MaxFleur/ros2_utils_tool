44466e71514860973d5cc87162111570bc316d42
MaxFleur <bakefish@web.de>
2025-10-06 14:51:45 +0200

    [fix][ui] Ok button was enabled for topic combo box widgets when there was no topic available

--- src/ui/InputWidgets/BagToImagesWidget.cpp
+++ src/ui/InputWidgets/BagToImagesWidget.cpp
@@ -51,7 +51,7 @@ BagToImagesWidget::BagToImagesWidget(Parameters::BagToImagesParameters& paramete
 
     // Generally, only enable this if the source bag, topic name and target dir line edit contain text
     enableOkButton(!m_parameters.sourceDirectory.isEmpty() &&
-                   !m_parameters.topicName.isEmpty() && !m_parameters.targetDirectory.isEmpty());
+                   !m_topicNameComboBox->currentText().isEmpty() && !m_parameters.targetDirectory.isEmpty());
 
     connect(formatComboBox, &QComboBox::currentTextChanged, this, &BagToImagesWidget::adjustWidgetsToChangedFormat);
     connect(advancedOptionsCheckBox, &QCheckBox::stateChanged, this, [this, advancedOptionsWidget] (int state) {
